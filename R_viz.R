3b3cec45b4e72488b52316bda96d1143fd1dd07b
Yaguang Wei <weiyg@hsph.harvard.edu>
2022-04-12 13:01:57 -0400

    Update R_viz.R

--- R_viz.R
+++ R_viz.R
@@ -27,7 +27,7 @@ uniam_results_sub <- readRDS(paste0(dir_data,'dat_sub.rds'))
 
 options(na.action = "na.pass")  # to not ignore NAs during plotting
 
-space <- '        '
+space <- '        '  # indentation
 
 ### subset pm25
 uniam_results_sub_pm25 <- uniam_results_sub[uniam_results_sub$exp=='pm25',]
@@ -55,7 +55,7 @@ uniam_results_sub_pm25$cat <- c(paste0(space,'Low'),
                                 paste0(space,'High'),
                                 paste0(space,'Medium to low'))
 
-uniam_results_sub_pm25 <- insertRows(uniam_results_sub_pm25, 1, new = NA)
+uniam_results_sub_pm25 <- insertRows(uniam_results_sub_pm25, 1, new = NA)  # insert black rows
 uniam_results_sub_pm25 <- insertRows(uniam_results_sub_pm25, 4, new = NA)
 uniam_results_sub_pm25 <- insertRows(uniam_results_sub_pm25, 4, new = NA)
 uniam_results_sub_pm25 <- insertRows(uniam_results_sub_pm25, 8, new = NA)
@@ -128,15 +128,16 @@ uniam_results_sub_no2 <- insertRows(uniam_results_sub_no2, 20, new = NA)
 ################## 2. Plot ##################
 pdf(paste0(dir_save,'Fig.pdf'), height = 7.5, width = 17)
 
-layout(matrix(c(1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3),byrow = TRUE,nrow = 1))
+layout(matrix(c(1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3),byrow = TRUE,nrow = 1))  # divide plot into three columns, specify column width
 
+# First column
 op = par(cex=0.8, font=1)
 forest(# Plot data
   x = uniam_results_sub_pm25$rr,
   ci.lb = uniam_results_sub_pm25$rr_ci_lower,
   ci.ub = uniam_results_sub_pm25$rr_ci_upper,
   annotate = F,
-  # Add lag names
+  # Add row names
   slab = uniam_results_sub_pm25$cat,
   # x-label
   xlab = "",
@@ -149,18 +150,19 @@ forest(# Plot data
   psize = c(rep(1.2,20),3.2), 
   pch = c(rep(16,20),18),
   cex.lab = 0.5)
+
+# Add text
 par(font = 2)
 text(-0.8, dim(uniam_results_sub_pm25)[1]+2, pos = 4, cex = 0.95, "Community-level")
 text(-0.8, dim(uniam_results_sub_pm25)[1]+1.5, pos = 4, cex = 0.95, "characteristic")
 text(-0.14, dim(uniam_results_sub_pm25)[1]+2, pos = 4, cex = 0.95, expression(paste(bold("Percent increase in risk of asthma hospitalization"))))
 text(-0.14, dim(uniam_results_sub_pm25)[1]+1.5, pos = 4, cex = 0.95, expression(paste(bold(" for each 1 "),bold("\u03bc"),bold("g/m")^bold("3"),bold(" increase in PM")[bold(2.5)],bold(' at lag 0-6 day'))))
 text(0.65, dim(uniam_results_sub_pm25)[1]+1.5, pos = 4, cex = 0.95, "p-value")
-
-### add text for the subgroups
-par(font=2)
 text(-0.8, c(21,17,13,9,5,1), pos=4, cex = 0.95, c("Population density","Average BMI","Percent of ever smokers",
                                                    "Distance to the nearest hospital",
                                                    "Neighborhood disadvantage level","Main analysis"))
+
+# Second column
 par(mar=(c(5+0.1,0,4+0.1,2+0.1)), font = 1)
 forest(# Plot data
   x = uniam_results_sub_o3$rr,
@@ -178,11 +180,14 @@ forest(# Plot data
   psize = c(rep(1.2,20),3.2), 
   pch = c(rep(16,20),18),
   cex.lab = 0.5)
+
+# Add text
 par(font = 2)
 text(-0.43, dim(uniam_results_sub_o3)[1]+2, pos = 4, cex = 0.95, expression(paste(bold("Percent increase in risk of asthma hospitalization"))))
 text(-0.43, dim(uniam_results_sub_o3)[1]+1.5, pos = 4, cex = 0.95, expression(paste(bold("     for each 1 ppb increase in O")[bold(3)],bold(' at lag 0-6 day'))))
 text(0.44, dim(uniam_results_sub_pm25)[1]+1.5, pos = 4, cex = 0.95, "p-value")
 
+# Third column
 par(mar=(c(5+0.1,0,4+0.1,2+0.1)), font = 1)
 forest(# Plot data
   x = uniam_results_sub_no2$rr,
@@ -200,6 +205,8 @@ forest(# Plot data
   psize = c(rep(1.2,20),3.2), 
   pch = c(rep(16,20),18),
   cex.lab = 0.5)
+
+# Add text
 par(font = 2)
 text(-0.25, dim(uniam_results_sub_no2)[1]+2, pos = 4, cex = 0.95, expression(paste(bold("Percent increase in risk of asthma hospitalization"))))
 text(-0.25, dim(uniam_results_sub_no2)[1]+1.5, pos = 4, cex = 0.95, expression(paste(bold("    for each 1 ppb increase in NO")[bold(2)],bold(' at lag 0-6 day'))))
